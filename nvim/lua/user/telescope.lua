05aac29a316a228bfcda7b1ce44593c6ec82a9ea
Sisir Ghimire Chettri <samratsisir294@gmail.com>
2022-12-12 14:45:47 -0800

    Update telescope

--- nvim/lua/user/telescope.lua
+++ nvim/lua/user/telescope.lua
@@ -23,7 +23,7 @@ telescope.setup({
     prompt_prefix = " ",
     selection_caret = " ",
     path_display = { "smart" },
-    file_ignore_patterns = { "^./.git/", "^node_modules/" },
+    file_ignore_patterns = { "^./.git/", "node_modules", "^./.nuxt/" },
     mappings = {
       i = {
         ["<esc>"] = actions.close,

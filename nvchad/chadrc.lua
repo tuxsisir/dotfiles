4d157125ed51ff689859a613ff14b800cec4ba67
Sisir Ghimire Chettri <contact@sisir.net>
2023-08-29 08:56:49 -0700

    bats: add helix

--- nvchad/chadrc.lua
+++ nvchad/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require("custom.highlights")
 
 M.ui = {
-	theme = "palenight",
-	theme_toggle = { "ayu_dark", "palenight" },
+	theme = "everforest",
+	theme_toggle = { "ayu_dark", "everforest" },
 
 	hl_override = highlights.override,
 	hl_add = highlights.add,

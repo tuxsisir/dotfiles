f4363af18bb523ad39f72761c002be82d203cfeb
Sisir Ghimire Chettri <contact@sisir.net>
2024-07-07 20:49:38 -0700

    Delete old conf

--- nvim/lua/user/highlights.lua
+++
@@ -1,8 +0,0 @@
-vim.cmd([[highlight IndentBlanklineChar guifg=#474747 gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineContextChar guifg=#828282 gui=nocombine]])
--- vim.cmd([[ let &t_8f="\<Esc>[38;2;%lu;%lu;%lum" ]])
--- vim.cmd([[ let &t_8b="\<Esc>[48;2;%lu;%lu;%lum" ]])
--- vim.api.nvim_set_hl(0, "@htmlArg", { italic = true })
--- vim.cmd([[ highlight htmlArg cterm=italic ]])
-vim.api.nvim_set_hl(0, "@comment", { fg = "#6e6e93", italic = true })
--- vim.api.nvim_set_hl(0, "htmlArg", { italic = true })

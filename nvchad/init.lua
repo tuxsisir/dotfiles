c8ac4af1da0875bac33788e9ee5c2cedb67ada67
Sisir Ghimire Chettri <contact@sisir.net>
2024-09-22 16:36:52 -0700

    use jedi

--- nvchad/init.lua
+++ nvchad/init.lua
@@ -19,11 +19,11 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 })
 
 -- call linter on leaving insert
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-  callback = function()
-    require("lint").try_lint()
-  end,
-})
+-- vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+--   callback = function()
+--     require("lint").try_lint()
+--   end,
+-- })
 
 -- Automatically refresh buffer after saving the file
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
--- nvchad/plugins.lua
+++ nvchad/plugins.lua
@@ -118,14 +118,14 @@ local plugins = {
 				python = { "ruff" },
 			}
 
-			local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+			-- local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-			vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-				group = lint_augroup,
-				callback = function()
-					lint.try_lint()
-				end,
-			})
+			-- vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+			-- 	group = lint_augroup,
+			-- 	callback = function()
+			-- 		lint.try_lint()
+			-- 	end,
+			-- })
 
 			vim.keymap.set("n", "<leader>lt", function()
 				lint.try_lint()
